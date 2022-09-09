43e5004a369a8b06c151641aab4303e8414d781c
isqua <isqua@isqua.ru>
2022-09-09 19:58:41 +0000

    feat: introduce component for input field

---
+++ src/shared/components/TextInput/TextInput.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { clx } from '../../utils/clx';
+import { FormHint } from '../FormHint';
+import { FormLabel } from '../FormLabel';
+import { ITextInputProps } from './TextInput.types';
+
+import styles from './TextInput.module.css';
+
+export function TextInput(props: ITextInputProps): React.ReactElement {
+  const {
+    className,
+    disabled,
+    hint,
+    label,
+    name,
+    type,
+    placeholder,
+    value,
+  } = props;
+  const htmlId = React.useId();
+
+  return (
+    <div className={clx(styles.container, className)}>
+      {label && <FormLabel htmlFor={htmlId}>{label}</FormLabel>}
+
+      <input
+        id={htmlId}
+        name={name}
+        value={value}
+        type={type}
+        placeholder={placeholder}
+        disabled={disabled}
+        className={styles.input}
+      />
+
+      {hint && <FormHint>{hint}</FormHint>}
+    </div>
+  );
+}
---
+++ src/shared/components/TextInput/TextInput.types.ts
@@ -0,0 +1,10 @@
+export interface ITextInputProps {
+  className?: string;
+  label?: string;
+  hint?: string;
+  value?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  name?: string;
+  type?: string;
+}
